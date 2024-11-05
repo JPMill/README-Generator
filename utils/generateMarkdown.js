e309387d6ab95e8c5a0ef4e4cd4a4957df1e3a60
Justin Miller <justinmiller617@gmail.com>
2024-11-04 22:53:54 -0600

    Updated all comments throughout code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-//TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (!license || license === 'None') {
@@ -12,7 +12,7 @@ function renderLicenseBadge(license) {
   return badges[license];
 }
 
-// TODO: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (!license || license === 'None') {
@@ -26,7 +26,7 @@ function renderLicenseLink(license) {
   return links[license];
 }
 
-// TODO: Create a function that returns the license section of README
+// Function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (!license || license === 'None') {
@@ -39,7 +39,7 @@ For more details, please refer to the ${renderLicenseLink(license)}.
   `;
 }
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
